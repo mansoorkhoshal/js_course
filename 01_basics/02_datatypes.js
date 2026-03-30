e12760c11b72c3c2534588d6663df8dd33feea7e
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2026-03-30 18:19:23 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,26 @@
 console.log("data types in a details inshallah");
+
+// alert(3 + 3) // we are using nodejs not browser
+
+/* today we'll be study the data type so i want to show all those which we use in this course max */
+
+let name = "string";
+let age = 18;
+let isLogedIn = false;
+let state;
+
+// this all are permitive datatypes
+// number => 2 to power 53 or 52
+// string => ""
+// bigInt 
+// boolean = true/fase
+// null => standalone value
+// undefined => when we dont enter any value
+// symbol => when we talk about uniqueness then we use the symbol data type
+
+// object
+
+console.log(typeof null);  // object 
+console.log(typeof undefined); // undefined
+
+
