e3a376a939310ef0624c58aa535c74c0ff64d3a7
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2026-04-01 16:47:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = 33;
+
+// console.log(typeof score); // number
+// console.log(typeof score); // number
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "mansoor";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "mansoor" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
