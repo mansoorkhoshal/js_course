d4bf35eb2f56ab1ba7893fbe3a5f3450cd48a4e5
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2026-04-03 16:58:29 +0000

    comparison of different value and different types to each other

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// this is the simple and clear comparison.
+// console.log(2 > 1);
+// console.log(2 >= 3);
+// console.log(2 != 3);
+// console.log(2 < 3);
+
+// this is also complected type of comparison sometimes its give confusion.
+// console.log("3" > 4);
+// console.log("05" >= 4);
+
+
+// avoid that type of comparison and write a clean and simple code very prefred.
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null != 0);
+
+console.log(undefined == 0);
+console.log(undefined != 0);
+console.log(undefined >= 0);
+
+
+// ===  strikly comparison which check both the value and type
+
+console.log("2" === 2);  // false
+
